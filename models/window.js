0064fbf55255ba77df991e1ef2fedda768a3740d
Abhivallabhaneni <112009679+Abhivallabhaneni@users.noreply.github.com>
2022-11-30 13:11:51 -0600

    added and modified the files

--- models/window.js
+++ models/window.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose")
 const windowSchema = mongoose.Schema({
-window_type: String,
-size: String,
-cost: Number
+window_type: {
+    type:String,
+    required:true
+},
+size: {
+    type:String,
+    required:true
+},
+cost: {
+    type:Number,
+    min:1,
+    max:100
+}
 })
 module.exports = mongoose.model("window",
 windowSchema)
--- routes/window.js
+++ routes/window.js
@@ -6,16 +6,23 @@ var router = express.Router();
 // router.get('/', function(req, res, next) {
 //   res.render('window', {title:'Search Results window'});
 // });
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  }
 /* GET detail window page */ 
 router.get('/detail', window_controlers.window_view_one_Page);
 /* GET window */ 
 router.get('/', window_controlers.window_view_all_Page );
 /* GET create window page */ 
-router.get('/create', window_controlers.window_create_Page); 
+router.get('/create',secured, window_controlers.window_create_Page); 
 /* GET create update page */ 
-router.get('/update', window_controlers.window_update_Page); 
+router.get('/update',secured, window_controlers.window_update_Page); 
 /* GET delete costume page */ 
-router.get('/delete', window_controlers.window_delete_Page); 
+router.get('/delete',secured, window_controlers.window_delete_Page); 
 // GET request for one window. 
 router.get('/window/:id', window_controlers.window_detail); 
 module.exports = router;
